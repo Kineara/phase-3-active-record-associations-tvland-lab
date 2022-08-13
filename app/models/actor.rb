1ae8979add32715b24fb28ee32ce827d2334a043
Lauren Hoock <laurenh8916@protonmail.com>
2022-08-12 23:26:05 -0600

    completed

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,15 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    chars = self.characters #returns array of instances
+    chars.each do |char|
+      return "#{char.name} - #{char.show.name}"
+    end
+  end
 end
